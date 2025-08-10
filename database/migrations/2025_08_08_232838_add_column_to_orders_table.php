223811300ecb1ff3ca5e54087d59c619c8858519
kravetso <okravets.info@gmail.com>
2025-08-10 14:32:29 +0300

    migration files changed

---
+++ database/migrations/2025_08_08_232838_add_column_to_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->string('customer_name');
+            $table->string('customer_phone');
+            $table->string('delivery_address');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn('customer_name');
+            $table->dropColumn('customer_phone');
+            $table->dropColumn('delivery_address');
+        });
+    }
+};
